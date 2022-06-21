ae762ea64c89fb5d19df057507df9dae9bc44a0a
Mohamed Saad <mohamedsaadmoustafa@gmail.com>
2022-06-21 12:25:12 +0200

    Create Activation_Functions.py

---
+++ Activation_Functions.py
@@ -0,0 +1,17 @@
+class Activation_Function:
+    def __init__(self, x):
+        self.x = np.array(x)
+        
+    def sigmoid(self):
+        return 1.0 / (1 + np.exp(-self.x))
+
+    def sigmoid_derivative(self):
+        return self.x * (1.0 - self.x)
+    
+    def __call__(self, method):
+        if method == 'sigmoid': return self.sigmoid(),
+        elif method == 'sigmoid_derivative': return self.sigmoid_derivative()
+        else: raise ValueError
+            
+#Activation_Function(0.1)('sigmoid')
+Activation_Function(0.1).sigmoid()
